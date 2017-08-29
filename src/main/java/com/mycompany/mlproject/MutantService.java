971d7d07c88c6281671402267a94f5a102fc6c41
augustourga <augustourga@hotmail.com>
2017-08-28 22:12:54 -0300

    disability bd connection

--- src/main/java/com/mycompany/mlproject/MutantService.java
+++ src/main/java/com/mycompany/mlproject/MutantService.java
@@ -15,11 +15,11 @@ import org.mongodb.morphia.Morphia;
  * @author augus
  */
 public class MutantService {
-    MongoClient client = new MongoClient("localhost", 4568);
-    Datastore datastore = new Morphia().createDatastore(client,"save");
+    //MongoClient client = new MongoClient("localhost", 4568);
+   // Datastore datastore = new Morphia().createDatastore(client,"save");
     
     public void addpost(Save save){
-    datastore.save(save);
+    //datastore.save(save);
     }
     
     public void setStats(Stats stats){
