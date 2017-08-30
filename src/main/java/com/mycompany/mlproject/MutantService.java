23ee9331327a26a64683e49023b83c3736a99d84
augustourga <augustourga@hotmail.com>
2017-08-30 11:11:41 -0300

    add false stats

--- src/main/java/com/mycompany/mlproject/MutantService.java
+++ src/main/java/com/mycompany/mlproject/MutantService.java
@@ -17,16 +17,26 @@ import org.mongodb.morphia.Morphia;
 public class MutantService {
     //MongoClient client = new MongoClient("localhost", 4568);
    // Datastore datastore = new Morphia().createDatastore(client,"save");
+    public int totalRecords;
+    public int mutantRecords;
+    
+    public void mutantService(){
+    totalRecords =0;
+    mutantRecords =0;
+    
+    }
     
     public void addpost(Save save){
     //datastore.save(save);
+    totalRecords++;
+    if( save.isMutant ){
+        mutantRecords++;
+    }
     }
     
     public void setStats(Stats stats){
     
             //query (total records, mutant records)
-            int totalRecords= 100;
-            int mutantRecords= 40;
            stats.count_human_dna= totalRecords;
            stats.count_mutant_dna= mutantRecords;
            stats.ratio(mutantRecords, totalRecords);
